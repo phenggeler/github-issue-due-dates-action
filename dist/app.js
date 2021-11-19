f396e1463526b2c6e83fb0c0810a4ff5579295f6
Peter Henggeler <phenggeler@peters-mbp.lan>
2021-11-19 10:24:37 -0700

    My first action is ready

--- dist/app.js
+++ dist/app.js
@@ -19717,7 +19717,7 @@ exports.run();
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.NEXT_MONTH_TAG_NAME = exports.OVERDUE_TAG_NAME = void 0;
 exports.OVERDUE_TAG_NAME = 'Overdue';
-exports.NEXT_MONTH_TAG_NAME = 'Due in 4 weeks';
+exports.NEXT_MONTH_TAG_NAME = 'Due this month.';
 
 
 /***/ }),
