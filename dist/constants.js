cde95014406ddab67190a6612ea5e50baebe8a77
phenggeler <10621768+phenggeler@users.noreply.github.com>
2021-11-19 09:53:43 -0700

    Update constants.js

--- dist/constants.js
+++ dist/constants.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.NEXT_WEEK_TAG_NAME = exports.OVERDUE_TAG_NAME = void 0;
+exports.NEXT_MONTH_TAG_NAME = exports.OVERDUE_TAG_NAME = void 0;
 exports.OVERDUE_TAG_NAME = 'Overdue';
-exports.NEXT_WEEK_TAG_NAME = 'Due in coming month';
+exports.NEXT_MONTH_TAG_NAME = 'Due in coming month';
